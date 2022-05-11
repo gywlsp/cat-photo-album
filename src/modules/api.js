0c3a72a3329f6ead93ad61a713d90cbcdf646588
gywlsp <gywls517@gmail.com>
2022-05-11 18:53:43 +0900

    feat: :sparkles: add function getNodeData

---
+++ src/modules/api.js
@@ -0,0 +1,16 @@
+const API_END_POINT =
+  "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
+
+export const getNodeData = async (nodeId) => {
+  try {
+    const res = await fetch(`${API_END_POINT}/${nodeId ? nodeId : ""}`);
+
+    if (!res.ok) {
+      throw new Error("서버가 에러를 반환했습니다.");
+    }
+
+    return await res.json();
+  } catch (e) {
+    throw new Error(`에러가 발생했습니다. ${e.message}`);
+  }
+};
