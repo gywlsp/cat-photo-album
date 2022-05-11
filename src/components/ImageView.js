30a0790339c4f070c0789682158545cbcd1cc6c0
gywlsp <gywls517@gmail.com>
2022-05-11 18:53:12 +0900

    feat: :sparkles: add component ImageView

---
+++ src/components/ImageView.js
@@ -0,0 +1,40 @@
+const IMAGE_PATH_PREFIX =
+  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
+
+export default function ImageView({ $app, initialState, onClose }) {
+  this.state = initialState;
+  this.$target = document.createElement("div");
+  this.$target.className = "Modal ImageView";
+
+  $app.appendChild(this.$target);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.onClose = onClose;
+
+  this.render = () => {
+    this.$target.innerHTML = `<div class="content">${
+      this.state ? `<img src="${IMAGE_PATH_PREFIX}${this.state}">` : ""
+    }</div>`;
+
+    this.$target.style.display = this.state ? "block" : "none";
+  };
+
+  this.$target.addEventListener("click", (e) => {
+    const $node = e.target.closest(".content");
+    if (!$node) {
+      this.onClose();
+    }
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      this.onClose();
+    }
+  });
+
+  this.render();
+}
